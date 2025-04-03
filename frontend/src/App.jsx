0adf9e4e47409104e37d82fd1c83a81477b202cf
Arjun Manoj <arjunmanoj5500@gmail.com>
2025-04-03 19:02:13 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind is Working!</h1>
+    </>
+  );
+}
+
+export default App;
